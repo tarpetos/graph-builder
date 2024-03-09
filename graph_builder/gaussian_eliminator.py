47949a6ed6682d5a70f64243e6cdd4a7afde88eb
Taras Hevlich <hevlichtaras@gmail.com>
2024-03-09 16:48:01 +0200

    Add system of linear equations helper data class

--- graph_builder/gaussian_eliminator.py
+++ graph_builder/gaussian_eliminator.py
@@ -13,36 +13,64 @@ class SLE:
         return (getattr(self, attr) for attr in vars(self))
 
     def __post_init__(self) -> None:
-        if len(self.MAIN_MATRIX) < 2:
-            raise ValueError("Matrix size is less than 2x2!")
+        self.__raise_conditional_exception(
+            len(self.MAIN_MATRIX) < 2, ValueError, "Matrix size is less than 2x2!"
+        )
+        self.__raise_conditional_exception(
+            len(self.MAIN_MATRIX) != len(self.COLUMN_VECTOR),
+            ValueError,
+            "Size of the column vector is not equal to the number of rows in the main matrix!",
+        )
+        self.__raise_conditional_exception(
+            any(len(row) != len(self.COLUMN_VECTOR) for row in self.MAIN_MATRIX),
+            ValueError,
+            "Matrix is not square!",
+        )
+        transposed_matrix = list(zip(*self.MAIN_MATRIX))
+        self.__raise_conditional_exception(
+            any(all(value == 0 for value in row) for row in self.MAIN_MATRIX)
+            or any(all(value == 0 for value in row) for row in transposed_matrix),
+            ValueError,
+            "System of linear equations has no real "
+            "solutions or it has infinite number of solutions!",
+        )
 
-        if len(self.MAIN_MATRIX) != len(self.COLUMN_VECTOR):
-            raise ValueError("Invalid matrix dimensions!")
-
-        matrix_is_square: bool = all(len(row) == len(self.COLUMN_VECTOR) for row in self.MAIN_MATRIX)
-        if not matrix_is_square:
-            raise ValueError("Matrix is not square!")
-
-        # first_row_size: int = len(self.MAIN_MATRIX[0])
-        # print([len(row) == first_row_size for row in self.MAIN_MATRIX[1:]])
-        # print(is_square)
-        # print(list(zip(*self.MAIN_MATRIX)))
+    @staticmethod
+    def __raise_conditional_exception(
+        condition: bool,
+        exception: type(Exception),
+        message: str,
+    ) -> None:
+        if condition:
+            raise exception(message)
 
 
 class GaussianEliminator:
     def __init__(self, main_matrix: Matrix, column_vector: Vector) -> None:
         self._matrix: Matrix = main_matrix
         self._vector: Vector = column_vector
+        self._augmented_matrix: Matrix = [
+            row.append(col_val) or row for row, col_val in zip(main_matrix, column_vector)
+        ]
+        self.solution_vector: Vector = [0.0 for _ in range(len(self._vector))]
 
-        # self.__is_solvable: bool = True
+    def convert_2_row_echelon_form(self) -> None:
+        self._straight_gait()
+        self._reversed_gait()
 
-    # def _parse_input(self) -> None:
-    #     if len(self._matrix) != len(self._vector):
-    #         self.__is_solvable = False
+    def _straight_gait(self) -> None:
+        ...
+
+    def _reversed_gait(self) -> None:
+        ...
 
 
 sle_matrix: SLE = SLE(
-    MAIN_MATRIX=[[1, 2], [4, 5]],
+    MAIN_MATRIX=[
+        [0, 2],
+        [1, 5],
+    ],
     COLUMN_VECTOR=[10, 11],
 )
 eliminator: GaussianEliminator = GaussianEliminator(*sle_matrix)
+eliminator.convert_2_row_echelon_form()
